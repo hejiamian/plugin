d253c7d1399421970988ef9d6ab33be63af04d57
hejiamian <hjm3774142@126.com>
2020-12-25 11:51:52 +0800

    traversal files

--- standardplugin/src/main/java/com/github/standardplugin/LogTransform.kt
+++ standardplugin/src/main/java/com/github/standardplugin/LogTransform.kt
@@ -6,7 +6,8 @@ import com.android.build.api.transform.Transform
 import com.android.build.api.transform.TransformInvocation
 import com.android.build.gradle.internal.pipeline.TransformManager
 import com.android.utils.FileUtils
-import org.objectweb.asm.ClassReader
+import proguard.classfile.ClassPool
+import java.io.File
 
 
 class LogTransform : Transform() {
@@ -25,26 +26,59 @@ class LogTransform : Transform() {
     }
 
     override fun transform(transformInvocation: TransformInvocation) {
-        super.transform(transformInvocation)
+//        super.transform(transformInvocation)
         val inputs = transformInvocation.inputs
         val outputProvider = transformInvocation.outputProvider
 
         inputs.forEach { input ->
-            input.jarInputs.forEach {
-                val dest = outputProvider.getContentLocation(it.name, it.contentTypes,
-                        it.scopes, Format.JAR)
-                println("jarInputs file name = ${it.file.name}")
-//                FileUtils.copyFile(it.file, dest)
-            }
-
             input.directoryInputs.forEach {
                 val dest = outputProvider.getContentLocation(it.name, it.contentTypes,
                         it.scopes, Format.DIRECTORY)
-                println("directoryInputs file name = ${it.name}")
+                traversal(it.file)
+                println("directory name = ${it.file.name}")
 //                val cr = ClassReader(it.file.name)
 //                cr.accept(LogClassVisitor(), ClassReader.SKIP_DEBUG)
-//                FileUtils.copyDirectory(it.file, dest)
+                FileUtils.copyDirectory(it.file, dest)
+            }
+
+            input.jarInputs.forEach {
+                val dest = outputProvider.getContentLocation(it.name, it.contentTypes,
+                        it.scopes, Format.JAR)
+                println("jar name = ${it.file.name}")
+                FileUtils.copyFile(it.file, dest)
             }
         }
+    }
+
+    private fun traversal(file: File) {
+        if (file.isFile){
+            println(file.name)
+        } else {
+            val files = file.listFiles()
+            files.forEach {
+                traversal(it)
+            }
+        }
+//
+//        try {
+//            Files.walkFileTree(path, object : SimpleFileVisitor<Path>() {
+//                override fun preVisitDirectory(dir: Path, attrs: BasicFileAttributes?): FileVisitResult {
+//                    if (dir === path) {
+//                        return FileVisitResult.CONTINUE
+//                    }
+//                    traversal(dir, depth)
+//                    return FileVisitResult.CONTINUE
+//                }
+//
+//                override fun visitFile(file: Path?, attrs: BasicFileAttributes?): FileVisitResult {
+//                    val file = path.toFile()
+//                    val name = file.name
+//                    println("file name = $name")
+//                    return FileVisitResult.CONTINUE
+//                }
+//            })
+//        } catch (ex: IOException) {
+//            ex.printStackTrace()
+//        }
     }
 }
